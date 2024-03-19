5c80071bdb69e9ed346849c492df78170d2cc613
Hadeer Abdelnasser <101938448+HadeerAbdelnasser204@users.noreply.github.com>
2024-03-19 22:44:32 +0200

    2 and 4

--- predicates.pl
+++ predicates.pl
@@ -1,5 +1,13 @@
 :-consult("data.pl").
 
+%SUB RULE%
+% get length
+% base case: empty List
+calcLength([], 0).
+%recursive
+calcLength([_|T],L):-
+   calcLength(T,L2),
+   L is L2 + 1.
 
 % RULES
 % 1:-
@@ -8,10 +16,21 @@
 list_order(CustomerId, Orders) :-
     customer(X,CustomerId),
     order(X, _, Orders).
-
+    
+% 2:-
+% Return number of orders of a specific customer.
+countOrdersOfCustomer(CustomerId,Count):-
+ list_order(CustomerId, Orders),
+ calcLength(Orders, Count).
 
 % 3:-
 % takes name, orderid -> matches the name with customer id  and with the customer id it gets , it gets the order
 getItemInOrderByID(X, OrderID , Items):-
     customer(CustomerId,X),
     order(CustomerId,OrderID, Items).
+
+% 4:-
+% Return num of items in a specific customer order
+getNumOfItems(CustomerName,OrderID,Count):-
+       getItemInOrderByID(CustomerName,OrderID,Items), % get List of items
+       calcLength(Items, Count).
