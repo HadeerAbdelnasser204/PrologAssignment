2a107f74ae08e462f32063956c187882e47d45ec
AsmaaElawady <asmaalwdy6@gmail.com>
2024-03-24 02:12:34 +0200

    added problem 1 and some updates

--- predicates.pl
+++ predicates.pl
@@ -13,11 +13,52 @@ calcLength([_|T],L):-
 
 % RULES
 % 1:-
-% returns only one order, needs modification to return all orders
-% base case: no orders by this name
-list_order(CustomerId, Orders) :-
-    customer(X,CustomerId),
-    order(X, _, Orders).
+% Main predicate to list all orders of a specific customer
+% Main predicate to list all orders of a specific customer
+list_orders(CustomerName, Orders) :-
+    customer(CustomerId, CustomerName),
+    get_orders(CustomerId, [], Orders),!. % Start with an empty list for the orders 
+
+% Recursive predicate to collect all orders of a specific customer
+get_orders(CustomerId, FoundOrders, Orders) :-
+    order(CustomerId, OrderID, Items), % Retrieve an order
+    not_inList(order(CustomerId, OrderID, _), FoundOrders), % Ensure order hasnt been added to list  yet
+    NewOrder = order(CustomerId, OrderID, Items),
+    append(FoundOrders, [NewOrder], UpdatedOrders), % Add the new order to the list of found orders
+    get_orders(CustomerId, UpdatedOrders, Orders). % Recursively process the rest of the orders
+
+% Base case: when there are no more orders left to process
+get_orders(_, Orders, Orders).
+
+% Utility predicate to check if an order is not a member of a list of orders
+
+% Base case: an order is not a member of an empty list
+not_inList(_, []). 
+
+% another base case: the order matches the head of the list
+not_inList(Order, [H|_]) :-
+    order_matches(Order, H), % If the current order matches, it means it is already a member
+    !,
+    fail.
+
+% Recursive case: check each element of the list
+not_inList(Order, [H|T]) :-
+    \+ order_matches(Order, H),  % Check if the current order matches the one we are looking for
+    not_inList(Order, T). % Recursively check the rest of the list
+
+% Utility function to append to list
+
+% base case: empty list
+append([], L, L).
+
+% recursive case: 
+append([H|T], L2, [H|NT]):-
+    append(T, L2, NT).
+
+% utility to compare two orders to each other , will be used above so if the equal then the order is in the list no need to append
+order_matches(order(CustomerId1, OrderID1, _), order(CustomerId2, OrderID2, _)) :-
+    CustomerId1 = CustomerId2,
+    OrderID1 = OrderID2.
     
 
 % 2:-
@@ -29,21 +70,21 @@ countOrdersOfCustomer(CustomerId,Count):-
 
 % 3:-
 % takes name, orderid -> matches the name with customer id  and with the customer id it gets , it gets the order
-getItemInOrderByID(X, OrderID , Items):-
+getItemsInOrderById(X, OrderID , Items):-
     customer(CustomerId,X),
-    order(CustomerId,OrderID, Items).
+    order(CustomerId,OrderID, Items),!.
 
 
 % 4:-
 % Return num of items in a specific customer order
 getNumOfItems(CustomerName,OrderID,Count):-
-    getItemInOrderByID(CustomerName,OrderID,Items), % get List of items
+    getItemsInOrderById(CustomerName,OrderID,Items), % get List of items
     calcLength(Items, Count).
 
 %P5
 %Calculate the price of a given order given Customer Name and order id
 calcPriceOfOrder(CustomerName, OrderID, TotalPrice):-
-    getItemInOrderByID(CustomerName, OrderID, Items),
+    getItemsInOrderById(CustomerName, OrderID, Items),
     calc(Items, TotalPrice).
 
 calc([], 0).
@@ -78,7 +119,7 @@ whyToBoycott(Item, Justification) :-
 % P8
 %Given an username and order ID, remove all the boycott items from this order.
 removeBoycottItemsFromAnOrder(CustomerName, OrderID, NewList):-
-    getItemInOrderByID(CustomerName, OrderID, Items),
+    getItemsInOrderById(CustomerName, OrderID, Items),
     remove(Items, NewList),!.
 
 remove([], []).
@@ -126,12 +167,11 @@ calcPrice(T, Temp, TotalPrice).
 
 %P11
 %calculate the difference in price between the boycott item and its alternative.
-getTheDiffInPriceBetProductAndAlt(Product, Alt, Diff) :-
+getTheDifferenceInPriceBetweenItemAndAlternative(Product, Alt, Diff) :-
     item(Product, BoycottedCompany, ProPrice),
     alternative(Product, Alt),
     item(Alt, _, AltPrice),
     Diff is ProPrice - AltPrice.
-
 
 % 12:- 
 %add
